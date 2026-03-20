19dac5f8f8357357c44560d6872ebc72ffac18de
jhuliangr <jhpiano9731@gmail.com>
2026-03-20 11:59:40 +0100

    Wrong commit

--- src/MainMenu/Settings/Settings.tsx
+++ src/MainMenu/Settings/Settings.tsx
@@ -2,17 +2,30 @@ import { Link } from 'react-router';
 import { CharacterSelector } from './CharacterSelector';
 import { SkinSelector } from './SkinSelector';
 
-export const Settings: React.FC = () => {
+export const 
+
+
+
+
+
+Settings: React.FC = () => {
   return (
     <div className="flex-1 flex items-center justify-around flex-col">
-      <div className="flex items-center justify-between gap-5">
+                                                                                                                            <div className="flex items-center justify-between gap-5">
         <Link to="/" className="bg-gray-300 p-2 rounded-lg text-black">
           Back
         </Link>
-        <CharacterSelector />
+                                                                                                                  <CharacterSelector />
       </div>
       <SkinSelector />
       {/* weapons */}
     </div>
-  );
+  
+
+
+
+
+
+
+);
 };
